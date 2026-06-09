0a4742dd65551d7b12ef66763d214cdd736a3122
turbillon50 <turbillon50@gmail.com>
2026-06-09 12:23:29 -0600

    fix: use props param in ImportCard

--- app/(admin)/importar/page.tsx
+++ app/(admin)/importar/page.tsx
@@ -148,7 +148,8 @@ type ImportCardProps = {
   onImported: (entry: HistoryEntry) => void
 }
 
-function ImportCard({ card, onImported }: ImportCardProps) {
+function ImportCard(props: ImportCardProps) {
+  const { card, onImported } = props;
   const [state, setState] = useState<ImportState>({
     stage: 'idle', file: null, preview: [], validCount: 0, errorCount: 0,
     progress: 0, result: null, errorMsg: '',
